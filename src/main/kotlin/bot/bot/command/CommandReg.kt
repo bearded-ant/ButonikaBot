5fec6f3855ac5f078eaa7b2d7b622eab8f5eba5b
ant <bearded.himenos@gmail.com>
2023-03-30 15:43:07 +0400

    command reg check is courier already registered

--- src/main/kotlin/bot/bot/command/CommandReg.kt
+++ src/main/kotlin/bot/bot/command/CommandReg.kt
@@ -1,6 +1,7 @@
 package bot.bot.command
 
 import com.google.firebase.database.FirebaseDatabase
+import firebase.FireBaseRepo
 import model.Courier
 import org.apache.logging.log4j.kotlin.Logging
 import org.telegram.telegrambots.meta.api.objects.Message
@@ -11,23 +12,26 @@ class CommandReg : Command("reg", "регистрация"), Logging {
 
         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
         val courierRef = database.getReference("courier")
+        val fireBaseRepo = FireBaseRepo(courierRef)
 
-        val courierNicName = message.from.userName ?: "anonymous"
-        val courierRealName = "name"
-        val courierId = message.chatId.toString()
+        if (!fireBaseRepo.isUserExist(message.chatId.toString())) {
+            val courierNicName = message.from.userName ?: "anonymous"
+            val courierRealName = "name"
+            val courierId = message.chatId.toString()
 
-        val courier: Courier = Courier(
-            id = courierId,
-            name = courierRealName,
-            nicName = courierNicName,
-            deliveryArea = 1
-        )
-        val courierMap = mutableMapOf<String, Courier>()
-        courierMap[message.chatId.toString()] = courier
+            val courier: Courier = Courier(
+                id = courierId,
+                name = courierRealName,
+                nicName = courierNicName,
+                deliveryArea = 1
+            )
+            val courierMap = mutableMapOf<String, Courier>()
+            courierMap[message.chatId.toString()] = courier
 
-        courierRef.setValueAsync(courierMap)
-
-        message.text = ("Добро пожаловать, $courierNicName!")
+            courierRef.setValueAsync(courierMap)
+            message.text = ("Добро пожаловать, $courierNicName!")
+        }
+        else message.text = ("Вы уже зарегистрированы")
         super.processMessage(absSender, message, strings)
     }
 }
--- src/main/kotlin/firebase/FireBaseRepo.kt
+++ src/main/kotlin/firebase/FireBaseRepo.kt
@@ -1,4 +1,27 @@
 package firebase
 
-class FireBaseRepo {
+import com.google.firebase.database.*
+import org.apache.logging.log4j.kotlin.Logging
+
+class FireBaseRepo(
+//    private val dbInstance: FirebaseDatabase,
+    private val dbReference: DatabaseReference
+) : Logging {
+
+    fun isUserExist(id: String): Boolean {
+        var result = false
+        val query: Query = dbReference.orderByChild("id").equalTo(id)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.hasChildren()) {
+                    result = true
+                } else logger.debug("снапшот пустой, нет такого id")
+            }
+
+            override fun onCancelled(error: DatabaseError?) {
+                logger.error(error?.message ?: "Что то полшло не так")
+            }
+        })
+        return result
+    }
 }
